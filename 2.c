af18378cc5587cb787dedd5a1a1146741c632365
Kio <kivan2@yandex.ru>
2021-09-21 00:17:08 +0300

    Second chance

---
+++ 2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char c, name[20] = {};
+    int i = 0;
+
+    printf("Hello, enter your name!\n");
+
+    while((c = getchar()) != EOF && i < 10){
+        name[i] = c;
+        i++;
+    }
+
+    printf("Hello,");
+    i = 0;
+    while(name[i] != '\0'){
+        printf("%c", name[i]);
+        i++;
+    }
+    printf("\n");
+
+    return 0;
+}
